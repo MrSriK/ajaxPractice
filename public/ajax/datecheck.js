12ab1df83276826c495a93d61dbbd11013f6b878
MrSriK <krishnansridhar99@gmail.com>
2021-08-03 17:48:03 +0530

    Date checking updated

--- public/ajax/datecheck.js
+++ public/ajax/datecheck.js
@@ -1,13 +1,29 @@
-$("#checkdate").click(function () {
+let params={};
+let validate_flg=true;
+function validateDate(){
+    validate_flg=true;
     let date = $("#date").val();
-    let checkdate = {
-        date: date
-    };
+    let validator = /\d{2}[-]\d{2}[-]\d{4}/;
+    if(date.match(validator)){
+        console.log("Date is Correct");
+        alert("Date is Correct");
+        params['date']=date;
+    }
+    else{
+        console.log("Please enter valid date");
+        alert("Please enter valid date");
+        validate_flg=false;
+    }
+}
 
+$("#checkdate").click(function (){
+    validateDate();
+    if(validate_flg){  
+        
     $.ajax({
         type: "POST",
         url: "/datecheck",
-        data: checkdate,
+        data: params,
         success: function (data) {
             alert("Date submitted");
         },
@@ -15,4 +31,9 @@ $("#checkdate").click(function () {
             console.log(err);
         }
     });
+    }
+
+    else{
+        alert("Fill the fields properly");
+    }
 });
--- routes/datecheck.js
+++ routes/datecheck.js
@@ -9,13 +9,6 @@ router.post("/",function (req,res,next){
     console.log(req.body);
     let date=req.body.date;
     console.log("Entered Date: "+date);
-    let validator = /\d{2}[-]\d{2}[-]\d{4}/;
-    if(date.match(validator)){
-        console.log("Date is Correct");
-    }
-    else{
-        console.log("Please enter valid date");
-    }
 });
 
 
